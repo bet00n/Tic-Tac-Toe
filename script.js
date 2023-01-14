80beea8a0a16d0381516ec42a380f592bd8c3c1e
bet00n <luka9pl@gmail.com>
2023-01-14 23:09:04 +0100

    Feat win conditions

--- script.js
+++ script.js
@@ -1,53 +1,88 @@
-const cells = document.querySelectorAll('.cell:not(.taken)');
-const playersTokenDisplay = document.querySelector('h2 span');
-
+const cells = document.querySelectorAll(".cell");
+const playersTokenDisplay = document.querySelector("h2 span");
+const resetBtn = document.querySelector("button");
 
 const xToken = '<i class="fa-solid fa-x"></i>';
 const circleToken = '<i class="fa-solid fa-o"></i>';
 let playersToken;
 let botsToken;
+let isGameActive = true;
+
+let isClickable = true;
+
+document.querySelector(".game-content").addEventListener(
+  "click",
+  (e) => {
+    if (!isClickable) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+  },
+  true
+);
+
+const checkForWin = (side) => {
+  const winningConditions = [
+    [cells[0], cells[1], cells[2]],
+    [cells[3], cells[4], cells[5]],
+    [cells[6], cells[7], cells[8]],
+    [cells[0], cells[3], cells[6]],
+    [cells[1], cells[4], cells[7]],
+    [cells[2], cells[5], cells[8]],
+    [cells[0], cells[4], cells[8]],
+    [cells[2], cells[4], cells[6]],
+  ];
+
+  for (i = 0; i < winningConditions.length; i++) {
+    if (winningConditions[i].every((cell) => cell.classList.contains(side))) {
+      winningConditions[i].forEach((cell) => cell.classList.add('won'));
+      console.log(side.toUpperCase() + ' WON!');
+      isGameActive = false;
+      break;
+    }
+  }
+};
 
 const handleCellClick = (clickedCellEl) => {
-    const clickedCell = cells[clickedCellEl.target.getAttribute('data-cell-index')];
-    if (clickedCell.classList.contains('taken')) {
-        alert ('You can\'t play there!');
-    } else {
-        clickedCell.classList.add('taken');
-        clickedCell.innerHTML = playersToken;
-        botMove();
-    }
-}
+  const clickedCell =
+    cells[clickedCellEl.target.getAttribute("data-cell-index")];
+  if (clickedCell.classList.contains("taken")) {
+    alert("You can't play there!");
+  } else {
+    clickedCell.classList.add("taken", "player");
+    clickedCell.innerHTML = playersToken;
+    isClickable = false;
+    checkForWin('player');
+    botMove();
+  }
+};
 
-cells.forEach((cell)=> {
-    cell.addEventListener('click', handleCellClick)
+cells.forEach((cell) => {
+  cell.addEventListener("click", handleCellClick);
 });
 
 const assingTokens = () => {
-    playersToken = (Math.random()-0.5)>0 ? xToken : circleToken;
-    playersTokenDisplay.innerHTML = playersToken;
-    botsToken = playersToken == xToken ? circleToken : xToken;
-}
+  playersToken = Math.random() - 0.5 > 0 ? xToken : circleToken;
+  playersTokenDisplay.innerHTML = playersToken;
+  botsToken = playersToken == xToken ? circleToken : xToken;
+};
 
 const botMove = () => {
-    const availableCells = document.querySelectorAll('.cell:not(.taken)');
-    console.log(availableCells);
-    const chosenCell = availableCells[Math.floor(Math.random() * availableCells.length)];
-    setTimeout (() => {
-        chosenCell.classList.add('taken');
-        chosenCell.innerHTML = botsToken;
-    }, 2000)
-    .then() {
-        freezeClic = true;
-    };
-}
+  const availableCells = document.querySelectorAll(".cell:not(.taken)");
+  if (isGameActive) {
+    const chosenCell =
+      availableCells[Math.floor(Math.random() * availableCells.length)];
+    setTimeout(() => {
+      chosenCell.classList.add("taken", "bot");
+      chosenCell.innerHTML = botsToken;
+      checkForWin('bot');
+      isClickable = true;
+    }, 2000);
+  }
+};
+
+resetBtn.addEventListener("click", () => {
+  console.log("Reset");
+});
 
 assingTokens();
-
-let freezeClic = false; // just modify that variable to disable all clics events
-
-document.addEventListener("click", e => {
-    if (freezeClic) {
-        e.stopPropagation();
-        e.preventDefault();
-    }
-}, true);
